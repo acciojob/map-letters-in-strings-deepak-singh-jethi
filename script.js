2ccbdaa37a396b00f8638242eeae584f114955d0
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-11-21 14:05:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+let str = prompt("enter input string");
+
+let ans = {};
+
+function check(str) {
+  for (let i = 0; i < str.length; i++) {
+    let arr = [i];
+    let key = str.charAt(i);
+
+    if (key in ans) {
+      ans[key].push(i);
+      
+    } else {
+      ans[key] = arr;
+    
+    }
+  }
+  return ans;
+}
+console.log(check(str));
